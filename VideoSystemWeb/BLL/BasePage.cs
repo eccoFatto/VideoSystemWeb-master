11bcc2e9aeccf8ae5baebef6f5d1dc8b292c12e9
Claudio <Claudio@NOTE-CLAUDIO>
2019-03-10 22:20:33 +0100

    ..

--- VideoSystemWeb/BLL/BasePage.cs
+++ VideoSystemWeb/BLL/BasePage.cs
@@ -13,8 +13,8 @@ namespace VideoSystemWeb.BLL
 {
     public class BasePage : System.Web.UI.Page
     {
-        public static string versione = "1.03";
-        public static string dataVersione = "07/03/2019";
+        public static string versione = "1.04";
+        public static string dataVersione = "10/03/2019";
 
         public List<Tipologica> listaRisorse
         {

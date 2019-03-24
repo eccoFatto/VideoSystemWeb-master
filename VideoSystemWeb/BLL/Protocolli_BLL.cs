3d9b398f32202e990b5a4665382fb1212b6277ea
Claudio <Claudio@NOTE-CLAUDIO>
2019-03-24 16:09:10 +0100

    recupero getprotocolli e template

--- VideoSystemWeb/BLL/Protocolli_BLL.cs
+++ VideoSystemWeb/BLL/Protocolli_BLL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using VideoSystemWeb.DAL;
 using VideoSystemWeb.Entity;
+using System.Configuration;
 namespace VideoSystemWeb.BLL
 {
     public class Protocolli_BLL
@@ -54,6 +55,72 @@ namespace VideoSystemWeb.BLL
             Esito esito = Protocolli_DAL.Instance.EliminaProtocollo(idProtocollo);
 
             return esito;
+        }
+
+        public int getProtocollo(ref Esito esito)
+        {
+            int iREt = Base_DAL.getProtocollo(ref esito);
+
+            return iREt;
+        }
+
+
+        public Esito resetProcotollo(int protocolloIniziale)
+        {
+            Esito esito = Base_DAL.resetProtocollo(protocolloIniziale);
+
+            return esito;
+        }
+
+        public string getNumeroProtocollo()
+        {
+            string ret = "";
+
+            ret = ConfigurationManager.AppSettings["NUMERO_PROTOCOLLO"];
+            Esito esito = new Esito();
+            int nProt = getProtocollo(ref esito);
+            if (esito.codice == Esito.ESITO_OK)
+            {
+                ret = ret.Replace("@anno", DateTime.Today.Year.ToString("0000"));
+                ret = ret.Replace("@protocollo", nProt.ToString(ConfigurationManager.AppSettings["FORMAT_NUMERO_PROTOCOLLO"]));
+            }
+            else
+            {
+                ret = "";
+            }
+            return ret;
+        }
+        public int getCodiceLavorazione(ref Esito esito)
+        {
+            int iREt = Base_DAL.getCodiceLavorazione(ref esito);
+
+            return iREt;
+        }
+
+        public Esito resetCodiceLavorazione(int codiceLavorazioneIniziale)
+        {
+            Esito esito = Base_DAL.resetCodiceLavorazione(codiceLavorazioneIniziale);
+
+            return esito;
+        }
+
+        public string getCodLavFormattato()
+        {
+            string ret = "";
+
+            ret = ConfigurationManager.AppSettings["CODICE_LAVORAZIONE"];
+            Esito esito = new Esito();
+            int codLav = getCodiceLavorazione(ref esito);
+            if (esito.codice == Esito.ESITO_OK)
+            {
+                ret = ret.Replace("@anno", DateTime.Today.Year.ToString("0000"));
+                ret = ret.Replace("@codiceLavorazione", codLav.ToString(ConfigurationManager.AppSettings["FORMAT_CODICE_LAVORAZIONE"]));
+            }
+            else
+            {
+                ret = "";
+            }
+            return ret;
         }
 
     }

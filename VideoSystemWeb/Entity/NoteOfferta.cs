0aa3062227f198e9e97032261edfe225a2e0533f
Claudio <Claudio@NOTE-CLAUDIO>
2019-04-14 16:51:37 +0200

    Gestione Tabella TAB_NOTE_OFFERTA

---
+++ VideoSystemWeb/Entity/NoteOfferta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VideoSystemWeb.Entity
+{
+    [Serializable]
+    public class NoteOfferta
+    {
+        private int id;
+        private int id_dati_agenda;
+        private string banca;
+        private int pagamento;
+        private string consegna;
+
+        public int Id { get => id; set => id = value; }
+        public int Id_dati_agenda { get => id_dati_agenda; set => id_dati_agenda = value; }
+        public string Banca { get => banca; set => banca = value; }
+        public int Pagamento { get => pagamento; set => pagamento = value; }
+        public string Consegna { get => consegna; set => consegna = value; }
+    }
+
+    
+}

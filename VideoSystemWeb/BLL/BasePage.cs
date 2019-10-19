a6aead7fbd388ad49f9bdf46df53a320aef0b826
Claudio <Claudio@NOTE-CLAUDIO>
2019-10-19 17:38:20 +0200

    modifiche slide pagina principale

--- VideoSystemWeb/BLL/BasePage.cs
+++ VideoSystemWeb/BLL/BasePage.cs
@@ -16,8 +16,8 @@ namespace VideoSystemWeb.BLL
 {
     public class BasePage : System.Web.UI.Page
     {
-        public static string versione = "1.47";
-        public static string dataVersione = "13/10/2019";
+        public static string versione = "1.48";
+        public static string dataVersione = "16/10/2019";
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public static T ValidaCampo<T>(WebControl campo, T defaultValue, bool isRequired, ref Esito esito)

2ce42cd226bce7c03f8c78f27f150c9163f081ae
eccoFatto <nicola.foti@gmail.com>
2019-11-30 15:45:57 +0100

    Gestione scadenzawio a metà

---
+++ VideoSystemWeb/Scadenzario/userControl/Scadenzario.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VideoSystemWeb.BLL;
+
+namespace VideoSystemWeb.Scadenzario.userControl
+{
+    public partial class Scadenzario : BasePage
+    {
+        //BasePage basePage = new BasePage();
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void ShowPopMessage(string messaggio)
+        {
+            messaggio = messaggio.Replace("'", "\\'");
+            messaggio = messaggio.Replace("\r\n", "<br/>");
+
+            Page page = HttpContext.Current.Handler as Page;
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "apripopupProt", script: "popupProt('" + messaggio + "')", addScriptTags: true);
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            CheckIsMobile();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRicercaProtocollo_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void gv_scadenze_RowDataBound(object sender, GridViewRowEventArgs e)
+        { }
+
+        protected void gv_scadenze_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+        }
+
+        protected void gv_scadenze_Sorting(object sender, GridViewSortEventArgs e)
+        {
+        }
+    }
+}
---
+++ VideoSystemWeb/Scadenzario/userControl/Scadenzario.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace VideoSystemWeb.Scadenzario.userControl
+{
+
+
+    public partial class Scadenzario
+    {
+
+        /// <summary>
+        /// Controllo lblScadenzario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblScadenzario;
+
+        /// <summary>
+        /// Controllo UpdatePanelRicerca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanelRicerca;
+
+        /// <summary>
+        /// Controllo ddl_TipoAnagrafica.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_TipoAnagrafica;
+
+        /// <summary>
+        /// Controllo ddl_CodiceAnagrafica.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_CodiceAnagrafica;
+
+        /// <summary>
+        /// Controllo txt_NumeroFattura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_NumeroFattura;
+
+        /// <summary>
+        /// Controllo ddlFatturaPagata.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlFatturaPagata;
+
+        /// <summary>
+        /// Controllo txt_DataFatturaDa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_DataFatturaDa;
+
+        /// <summary>
+        /// Controllo txt_DataFatturaA.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_DataFatturaA;
+
+        /// <summary>
+        /// Controllo txt_DataScadenzaDa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_DataScadenzaDa;
+
+        /// <summary>
+        /// Controllo txt_DataScadenzaA.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_DataScadenzaA;
+
+        /// <summary>
+        /// Controllo btnRicercaScadenza.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRicercaScadenza;
+
+        /// <summary>
+        /// Controllo BtnPulisciCampiRicerca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnPulisciCampiRicerca;
+
+        /// <summary>
+        /// Controllo gv_scadenze.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gv_scadenze;
+
+        /// <summary>
+        /// Controllo hf_tipoOperazione.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hf_tipoOperazione;
+    }
+}

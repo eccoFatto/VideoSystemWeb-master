96b88f05403e0af0e3101e086f6eb8fd382201ac
Claudio <Claudio@NOTE-CLAUDIO>
2019-12-10 18:59:21 +0100

    versione

--- VideoSystemWeb/BLL/BasePage.cs
+++ VideoSystemWeb/BLL/BasePage.cs
@@ -16,8 +16,8 @@ namespace VideoSystemWeb.BLL
 {
     public class BasePage : System.Web.UI.Page
     {
-        public static string versione = "1.56";
-        public static string dataVersione = "29/11/2019";
+        public static string versione = "1.57";
+        public static string dataVersione = "07/12/2019";
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public static T ValidaCampo<T>(WebControl campo, T defaultValue, bool isRequired, ref Esito esito)

383f8ba9ef943fcd3457fb0a5d78f0aba9a48c5a
claudio.calderai <claudio.calderai@gmail.com>
2020-01-17 19:03:45 +0100

    versione e deploy

--- VideoSystemWeb/BLL/BasePage.cs
+++ VideoSystemWeb/BLL/BasePage.cs
@@ -16,8 +16,8 @@ namespace VideoSystemWeb.BLL
 {
     public class BasePage : System.Web.UI.Page
     {
-        public static string versione = "1.63";
-        public static string dataVersione = "15/01/2020";
+        public static string versione = "1.64";
+        public static string dataVersione = "17/01/2020";
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public static T ValidaCampo<T>(WebControl campo, T defaultValue, bool isRequired, ref Esito esito)

9fb79f03608041a90a1f75d011925ba7e1c4e9d8
Claudio Calderai <claudio.calderai@gmail.com>
2020-05-08 00:56:10 +0200

    modifiche a report lavoratori digiornata

--- VideoSystemWeb/REPORT/ReportCollaboratoriPerGiornata.aspx.cs
+++ VideoSystemWeb/REPORT/ReportCollaboratoriPerGiornata.aspx.cs
@@ -40,34 +40,88 @@ namespace VideoSystemWeb.REPORT
             DateTime dataTmp = Convert.ToDateTime(tbDataRicerca.Text.Trim());
             string sDataTmp = dataTmp.ToString("yyyy-MM-dd") + "T00:00:00.000";
 
-            string queryRicerca = "select dal.descrizione as qualifica,ac.cognome as cognome_operatore, ac.nome as nome_operatore, da.codice_lavoro,produzione,lavorazione " +
-            "from[dbo].[tab_dati_agenda] da " +
-            "left join tipo_colonne_agenda ca " +
-            "on da.id_colonne_agenda = ca.id " +
-            "left join tipo_stato ts " +
-            "on da.id_stato = ts.id " +
-            "left join tipo_tipologie tt " +
-            "on da.id_tipologia = tt.id " +
-            "left join dati_lavorazione dl " +
-            "on dl.idDatiAgenda = da.id " +
-            "left join dati_articoli_lavorazione dal " +
-            "on dl.id = dal.idDatiLavorazione " +
-            "left join anag_collaboratori ac " +
-            "on dal.idCollaboratori = ac.id " +
-            "where ac.cognome is not null and dal.descrizione<> 'Diaria' " +
-            "and data_inizio_lavorazione <= '@dataElaborazione' and data_fine_lavorazione >= '@dataElaborazione' " +
-            "group by dal.descrizione ,ac.cognome, ac.nome, da.codice_lavoro,produzione,lavorazione " +
-            "order by qualifica,ac.cognome, ac.nome, codice_lavoro";
-            queryRicerca = queryRicerca.Replace("@dataElaborazione", sDataTmp);
-
-            queryRicerca = queryRicerca.Replace("@dataElaborazione", sDataTmp);
-
-            Esito esito = new Esito();
-            DataTable dtCollaboratori = Base_DAL.GetDatiBySql(queryRicerca, ref esito);
-
-            Session["TaskTableCollaboratori"] = dtCollaboratori;
-            gv_Collaboratori.DataSource = Session["TaskTableCollaboratori"];
+            gv_Collaboratori.DataSource = null;
             gv_Collaboratori.DataBind();
+
+            // VEDO SE ESISTONO LAVORAZIONI CON COLLABORATORI INGAGGIATI PER LA GIORNATA SELEZIONATA
+            string queryRicercaLavorazioniDelGiorno = "select da.codice_lavoro,produzione,lavorazione " +
+                "from[dbo].[tab_dati_agenda] da " +
+                "left join tipo_colonne_agenda ca " +
+                "on da.id_colonne_agenda = ca.id " +
+                "left join tipo_stato ts " +
+                "on da.id_stato = ts.id " +
+                "left join tipo_tipologie tt " +
+                "on da.id_tipologia = tt.id " +
+                "left join dati_lavorazione dl " +
+                "on dl.idDatiAgenda = da.id " +
+                "left join dati_articoli_lavorazione dal " +
+                "on dl.id = dal.idDatiLavorazione " +
+                "left join anag_collaboratori ac " +
+                "on dal.idCollaboratori = ac.id " +
+                "where ac.cognome is not null " +
+                "and dal.descrizione <> 'Diaria' " +
+                "and data_inizio_lavorazione <= '@dataElaborazione' and data_fine_lavorazione >= '@dataElaborazione' " +
+                "group by da.codice_lavoro,produzione,lavorazione " +
+                "order by codice_lavoro";
+            queryRicercaLavorazioniDelGiorno = queryRicercaLavorazioniDelGiorno.Replace("@dataElaborazione", sDataTmp);
+            Esito esito = new Esito();
+            DataTable dtLavorazioniDelGiorno = Base_DAL.GetDatiBySql(queryRicercaLavorazioniDelGiorno, ref esito);
+            Session["TaskTableLavorazioniDelGiorno"] = dtLavorazioniDelGiorno;
+
+            if (dtLavorazioniDelGiorno!=null && dtLavorazioniDelGiorno.Rows!=null && dtLavorazioniDelGiorno.Rows.Count>0)
+            {
+                // CREO L'ELENCO DELLE COLONNE CON LE LAVORAZIONI TROVATE
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Qualifica", System.Type.GetType("System.String"));
+                foreach (DataRow rigaLavorazioni in dtLavorazioniDelGiorno.Rows)
+                {
+                    dt.Columns.Add(rigaLavorazioni["codice_lavoro"].ToString() + " - " + rigaLavorazioni["produzione"].ToString() + " - " + rigaLavorazioni["lavorazione"].ToString(), System.Type.GetType("System.String"));
+                }
+                // TIRO FUORI L'ELENCO DELLE QUALIFICHE PRESENTI
+                string ricercaQualifiche = "select dal.descrizione as qualifica " +
+                    "from[dbo].[tab_dati_agenda] da " +
+                    "left join tipo_colonne_agenda ca " +
+                    "on da.id_colonne_agenda = ca.id " +
+                    "left join tipo_stato ts " +
+                    "on da.id_stato = ts.id " +
+                    "left join tipo_tipologie tt " +
+                    "on da.id_tipologia = tt.id " +
+                    "left join dati_lavorazione dl " +
+                    "on dl.idDatiAgenda = da.id " +
+                    "left join dati_articoli_lavorazione dal " +
+                    "on dl.id = dal.idDatiLavorazione " +
+                    "left join anag_collaboratori ac " +
+                    "on dal.idCollaboratori = ac.id " +
+                    "where ac.cognome is not null " +
+                    "and dal.descrizione <> 'Diaria' " +
+                    "and data_inizio_lavorazione <= '@dataElaborazione' and data_fine_lavorazione >= '@dataElaborazione' " +
+                    "group by dal.descrizione " +
+                    "order by dal.descrizione";
+                ricercaQualifiche = ricercaQualifiche.Replace("@dataElaborazione", sDataTmp);
+                esito = new Esito();
+                DataTable dtQualifiche = Base_DAL.GetDatiBySql(ricercaQualifiche, ref esito);
+                if (dtQualifiche != null && dtQualifiche.Rows != null && dtQualifiche.Rows.Count > 0)
+                {
+                    // TIRO FUORI L'ELENCO DELLE RIGHE, UNA PER OGNI QUALIFICA TROVATA
+                    foreach (DataRow rigaQualifiche in dtQualifiche.Rows)
+                    {
+                        DataRow dr;
+                        dr = dt.NewRow();
+                        dr[0] = rigaQualifiche["qualifica"].ToString();
+
+                        for (int i = 1; i < dt.Columns.Count; i++)
+                        {
+                            dr[i] = "qualifiche Lavorazione " + i.ToString();
+                        }
+
+                        dt.Rows.Add(dr);
+                    }
+                    gv_Collaboratori.DataSource = dt;
+                    gv_Collaboratori.DataBind();
+                }
+
+
+            }
 
         }
 
